38fa01091197df99b87cd2a1663fb4bbce95117a
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-10-03 14:21:04 -0500

    - Added 2 more solutions - Added simple input for selecting the solution

--- stock_picker.rb
+++ stock_picker.rb
@@ -8,7 +8,7 @@
 # - You need to buy before you can sell
 # - Pay attention to edge cases like when the lowest day is the last day or the highest day is the first day.
 #
-# Solution 1:
+# Solution 1
 #
 def stock_picker(stock_prices)
   output = []
@@ -23,12 +23,31 @@ def stock_picker(stock_prices)
   }
   output
 end
+
 #
-# Solution 2
+# Solution 2:
 #
+def stock_picker2(prices)
+  prices.each_with_index.map { |price, index| [price, index] }
+        .combination(2)
+        .map { |a, b| [a, b, b[0] - a[0]] }
+        .max_by { |a, b, c| c }
+        .slice(0, 2)
+        .map { |a| a[1] }
+end
+
 #
 # Solution 3
 #
+def stock_picker3(prices)
+  best = [0, 0]
+  prices.each_with_index do |price, index|
+    prices[index..].each_with_index do |price2, index2|
+      best = [index, index2 + index] if price2 - price > prices[best[1]] - prices[best[0]]
+    end
+  end
+  best
+end
 
 # ask user if run a generic test
 p 'Do you want to run a generic test or a custom test?'
@@ -38,14 +57,29 @@ stock_prices = []
 case gets.chop
 when '1'
   stock_prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
-  best_days = stock_picker(stock_prices)
 when '2'
   p 'Enter the stock prices separated by space'
   stock_prices = gets.chop.split(' ').map(&:to_i)
-  best_days = stock_picker(stock_prices)
 else
   p 'Invalid input'
 end
+
+# ask user what solution to run
+p 'Which solution do you want to run?'
+p 'Enter 1, 2 or 3'
+case gets.chop
+when '1'
+  best_days = stock_picker(stock_prices)
+when '2'
+  best_days = stock_picker2(stock_prices)
+when '3'
+  best_days = stock_picker3(stock_prices)
+else
+  p 'Invalid input'
+end
+
+p "input: #{stock_prices} output: #{best_days}"
+p 'Best days to buy and sell:'
 p "Best day to buy is the #{best_days[0] + 1}th day and best day to sell is the #{best_days[1] + 1}th day"
 p "with a profit of $#{stock_prices[best_days[1]] - stock_prices[best_days[0]]}"
 p "buying at $#{stock_prices[best_days[0]]} and selling at $#{stock_prices[best_days[1]]}"
